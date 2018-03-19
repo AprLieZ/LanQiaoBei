fbbc14c8317caee9c29883c9f8fdd358cbd79887
AprLieZ <289512298@qq.com>
2018-03-19 17:54:33 +0800

    java代码

--- src/基础练习/b2_01字串/Main2.java
+++ src/基础练习/b2_01字串/Main2.java
@@ -1,10 +1,17 @@
 package 基础练习.b2_01字串;
 
 /**
- * Description:算法
+ * Description:解法2 - 算法
  * User: zhenghb03
  * Date: 2018-03-16
  */
 public class Main2 {
-
+    public static void main(String[] args) {
+        for(int a=0;a<=1;a++)
+            for(int b=0;b<=1;b++)
+                for(int c=0;c<=1;c++)
+                    for(int d=0;d<=1;d++)
+                        for(int e=0;e<=1;e++)
+                            System.out.printf("%d%d%d%d%d\n",a,b,c,d,e);
+    }
 }
